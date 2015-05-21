f2942c3c4bc2e48c36316236f80e70e1900b68e5
pytlab <shaozhengjiang@gmail.com>
2015-05-21 20:47:42 +0800

    Initial commit

--- footer_meeting.php
+++ footer_meeting.php
@@ -2,25 +2,26 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>无标题文档</title>
+<title></title>
 
 </head>
 
-<body>
-	<div id="footer_meeting_wrapper" class="clearfix">
-    <div id="footer_meeting">
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="people.php">People</a></li>
-        <li><a href="/features/">News</a></li>
-        <li><a href="/download/">Hightlights</a></li>
-        <li><a href="/pro/">Publications</a></li>
-        <li><a href="/doc/">Courses</a></li>
-        <li><a href="/doc/">Meeting</a></li>
-        <li><a href="/contact/">Contact</a></li>
-      </ul>
-      <p class="copyright">&copy; Copyright <a href="index.php" target="_blank">Center for Computational Chemistry at ECUST</a>. All rights reserved.</p>
+<html>
+  <body>
+	  <div id="footer_meeting_wrapper" class="clearfix">
+      <div id="footer_meeting">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="people.php">People</a></li>
+          <li><a href="/features/">News</a></li>
+          <li><a href="/download/">Hightlights</a></li>
+          <li><a href="/pro/">Publications</a></li>
+          <li><a href="/doc/">Courses</a></li>
+          <li><a href="/doc/">Meeting</a></li>
+          <li><a href="/contact/">Contact</a></li>
+        </ul>
+        <p class="copyright">&copy; Copyright <a href="index.php" target="_blank">Center for Computational Chemistry at ECUST</a>. All rights reserved.</p>
+      </div>
     </div>
-  </div>
-</body>
+  </body>
 </html>
--- index.php
+++ index.php
@@ -1,45 +1,43 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<?php 
-	include_once("top_index.php");
-?>
-<head>
-	<!--<title>Homepage - CCC</title>-->
+  <?php 
+    include_once("top_index.php");
+  ?>
+  <head>
+    <!--<title>Homepage - CCC</title>-->
     <title><?php echo $title;?></title>
     <style>
-    	* { margin:0; padding:0; word-wrap: break-word; }		
+        * { margin:0; padding:0; word-wrap: break-word; }
     </style>
     <link href="./css/main.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
+  </head>
+  <body>
     <div id="body">
-        <!--左侧-->
-        <div id="body_left">
-            	
-            <div id="news">
-				<?php
-                     include("photo_change.php");//include_once("photo_change.php");
-                ?> 
-                <div class="area_news">
-      					
-                    <div id="news_list" style="word-wrap:break-word;">
-        		        <p>
-                            	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The Cambridge University Centre for Computational Chemistry groups theoretically-minded members of the Cambridge Department of Chemistry in premises on the recently refurbished third floor of the Department. Around 50 members, comprising staff, research fellows, postdoctoral associates, postgraduate students, and visiting scientists from all over the world, work on many aspects of theoretical and computational chemistry.
-					    </p>
-                    </div>	
-                </div>
-                <div id="forume">
-                    <h3 style="background:url(<?php echo $sci_highlight;?>) no-repeat;"></h3>
-                    <?php include_once("highlights_index.php");?>
-                </div>
-             </div>
+      <!--左侧-->
+      <div id="body_left">   
+        <div id="news">
+          <?php
+              include("photo_change.php");//include_once("photo_change.php");
+          ?> 
+          <div class="area_news">
+            <div id="news_list" style="word-wrap:break-word;">
+              <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The Cambridge University Centre for Computational Chemistry groups theoretically-minded members of the Cambridge Department of Chemistry in premises on the recently refurbished third floor of the Department. Around 50 members, comprising staff, research fellows, postdoctoral associates, postgraduate students, and visiting scientists from all over the world, work on many aspects of theoretical and computational chemistry.
+              </p>
+            </div>    
+          </div>
         </div>
-        <!--右侧-->
-        <?php include_once("body_right.php");?> 
+        <div id="forume">
+          <h3 style="background:url(<?php echo $sci_highlight;?>) no-repeat;"></h3>
+          <?php include_once("highlights_index.php");?>
+        </div>
+      </div>
+      <!--右侧-->
+      <?php include_once("body_right.php");?> 
     </div>
     
     <div id="foot">
-        <?php include_once("footer.php");?>
+      <?php include_once("footer.php");?>
     </div>
-</body>
+  </body>
 </html>
