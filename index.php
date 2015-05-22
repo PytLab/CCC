ec5e6fffb9f4290e4feb3711c2acf15b2b56ee98
pytlab <shaozhengjiang@gmail.com>
2015-05-22 15:01:57 +0800

    Modify indent

--- index.php
+++ index.php
@@ -1,44 +1,42 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-  <meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
-  <?php 
-    include_once("top_index.php");
-  ?>
-  <head>
-    <!--<title>Homepage - CCC</title>-->
-    <title><?php echo $title;?></title>
-    <style>
-        * { margin:0; padding:0; word-wrap: break-word; }
-    </style>
-    <link href="./css/main.css" rel="stylesheet" type="text/css" />
-  </head>
-  <body>
-    <div id="body">
-      <!--左侧-->
-      <div id="body_left">   
-        <div id="news">
-          <?php
-              include("photo_change.php");//include_once("photo_change.php");
-          ?> 
-          <div class="area_news">
-            <div id="news_list" style="word-wrap:break-word;">
-              <p>
-                <?php echo $intro;?>
-              </p>
-            </div>    
-          </div>
+    <meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
+    <?php 
+        include_once("top_index.php");
+    ?>
+    <head>
+        <!--<title>Homepage - CCC</title>-->
+        <title><?php echo $title;?></title>
+        <style>
+            * { margin:0; padding:0; word-wrap: break-word; }
+        </style>
+        <link href="./css/main.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div id="body">
+            <!--左侧-->
+            <div id="body_left">   
+                <div id="news">
+                    <?php
+                        include("photo_change.php");//include_once("photo_change.php");
+                    ?> 
+                    <div class="area_news">
+                        <div id="news_list" style="word-wrap:break-word;">
+                            <p><?php echo $intro;?></p>
+                        </div>    
+                    </div>
+                </div>
+                <div id="forume">
+                    <h3 style="background:url(<?php echo $sci_highlight;?>) no-repeat;"></h3>
+                    <?php include_once("highlights_index.php");?>
+                </div>
+            </div>
+            <!--右侧-->
+            <?php include_once("body_right.php");?> 
         </div>
-        <div id="forume">
-          <h3 style="background:url(<?php echo $sci_highlight;?>) no-repeat;"></h3>
-          <?php include_once("highlights_index.php");?>
-        </div>
-      </div>
-      <!--右侧-->
-      <?php include_once("body_right.php");?> 
-    </div>
     
-    <div id="foot">
-      <?php include_once("footer.php");?>
-    </div>
-  </body>
+        <div id="foot">
+            <?php include_once("footer.php");?>
+        </div>
+    </body>
 </html>
--- top_index.php
+++ top_index.php
@@ -1,91 +1,84 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-  <?php
-    date_default_timezone_set(PRC);
-    //@session_start(); //session在change_language.php中start
-    include_once("./conn/conn.php"); 
-    include_once("./function.php");
-    include_once("./get_language.php"); //识别浏览器语言
-    include_once("./change_language.php");  //通过点击链接切换语言
-  ?>
-  <meta http-equiv="Content-Type" content="text/html"; charset="utf-8"/>
-  <link href="./css/main.css" rel="stylesheet" type="text/css" />
-  <link rel="shortcut icon" href="icon.ico" />
-  </head>
+    <head>
+    <?php
+        date_default_timezone_set(PRC);
+        //@session_start(); //session在change_language.php中start
+        include_once("./conn/conn.php"); 
+        include_once("./function.php");
+        include_once("./get_language.php"); //识别浏览器语言
+        include_once("./change_language.php");  //通过点击链接切换语言
+    ?>
+    <meta http-equiv="Content-Type" content="text/html"; charset="utf-8"/>
+    <link href="./css/main.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="icon.ico" />
+    </head>
 
-  <body>
-    <div id="main">
-      <div id="head">
-        <div id="head_top">
-          <div id="search">
-            <?php
-              include_once("search_inweb.html");
-            ?>
-          </div>
-          <div id="logo"><a href="index.php"></a></div>
-        </div>
-        <div id="head_bottom">
-          <div id="head_nav">
-            <ul id="menu">
-              <li><a href="index.php" ><?php echo $home?></a>
-                <ul class="sub_menu">
-                </ul>
-              </li>
-              <li><a href="people.php" ><?php echo $people?></a>
-                <ul class="sub_menu">
-                  <li><a href="people.php#research_staff" ><?php echo $teachers?></a></li>
-                  <li><a href="people.php#graduate" ><?php echo $graduates?></a></li>
-                  <li><a href="people.php#graduated" ><?php echo $graduated?></a></li>
-                </ul>
-              </li>
-              <li><a href="news_list.php" ><?php echo $news?></a>
-              </li>
-              <li><a href="highlights_list.php" ><?php echo $highlights?></a>
-                <ul class="sub_menu">
-                  <li><a href="highlights_detail.php" ><?php echo $latest?></a></li>
-                  <li><a href="highlights_list.php" ><?php echo $high_list?></a></li>
-                </ul>
-              </li>
-              <li><a href="publications_list.php" ><?php echo $pubs?></a>
-              </li>
-              <li><a href="course_list.php" ><?php echo $courses?></a>
-                <ul class="sub_menu">
-                <?php
-                  $sql_acc=mysql_query("select * from tb_user where usernc = '".$_SESSION["unc"]."'");
-                  $info_acc=mysql_fetch_array($sql_acc);
-                  if($info_acc!="")
-                  {
-                ?>
-                  <li><a href="course_list.php" ><?php echo $course_list?></a></li>
-                  <li><a href="course_login.php?courseid=<?php echo $info_acc[courseid];?>" ><?php echo $course_acc?></a></li>   
-                <?php
-                  }
-                  else
-                  {
-                ?>
+    <body>
+        <div id="main">
+            <div id="head">
+                <div id="head_top">
+                    <div id="search">
+                        <?php include_once("search_inweb.html");?>
+                    </div>
+                    <div id="logo"><a href="index.php"></a></div>
+                </div>
+                <div id="head_bottom">
+                    <div id="head_nav">
+                        <ul id="menu">
+                            <li><a href="index.php" ><?php echo $home?></a>
+                                <ul class="sub_menu"></ul>
+                            </li>
+                            <li><a href="people.php" ><?php echo $people?></a>
+                                <ul class="sub_menu">
+                                    <li><a href="people.php#research_staff" ><?php echo $teachers?></a></li>
+                                    <li><a href="people.php#graduate" ><?php echo $graduates?></a></li>
+                                    <li><a href="people.php#graduated" ><?php echo $graduated?></a></li>
+                                </ul>
+                            </li>
+                            <li><a href="news_list.php" ><?php echo $news?></a></li>
+                            <li><a href="highlights_list.php" ><?php echo $highlights?></a>
+                                <ul class="sub_menu">
+                                    <li><a href="highlights_detail.php" ><?php echo $latest?></a></li>
+                                    <li><a href="highlights_list.php" ><?php echo $high_list?></a></li>
+                                </ul>
+                            </li>
+                            <li><a href="publications_list.php" ><?php echo $pubs?></a></li>
+                            <li><a href="course_list.php" ><?php echo $courses?></a>
+                                <ul class="sub_menu">
+                                    <?php
+                                        $sql_acc=mysql_query("select * from tb_user where usernc = '".$_SESSION["unc"]."'");
+                                        $info_acc=mysql_fetch_array($sql_acc);
+                                        if($info_acc!="")
+                                        {
+                                    ?>
+                                    <li><a href="course_list.php" ><?php echo $course_list?></a></li>
+                                    <li><a href="course_login.php?courseid=<?php echo $info_acc[courseid];?>" ><?php echo $course_acc?></a></li>   
+                                    <?php
+                                        }
+                                        else
+                                        {
+                                    ?>
 										
-                <?php
-                  }
-                ?>
-                </ul>
-              </li>
-              <li><a href="meeting.php" ><?php echo $meeting?></a>
-              </li>
-              <li><a href="contact.php" ><?php echo $contact?></a>
-              </li>
-              <div id="language_change">
-                <a href="<?php if(empty($_SERVER['QUERY_STRING']))
-                                 echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?lang='.$get_lang;
-                               else
-                                 echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'].'&lang='.$get_lang;
-                         ?>" target="_self"><?php echo $lan?></a>
-              </div>
-            </ul>
+                                    <?php
+                                        }
+                                    ?>
+                                </ul>
+                            </li>
+                            <li><a href="meeting.php" ><?php echo $meeting?></a></li>
+                            <li><a href="contact.php" ><?php echo $contact?></a></li>
+                            <div id="language_change">
+                                <a href="<?php if(empty($_SERVER['QUERY_STRING']))
+                                                   echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?lang='.$get_lang;
+                                               else
+                                                   echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'].'&lang='.$get_lang;
+                                         ?>" target="_self"><?php echo $lan?></a>
+                            </div>
+                        </ul>
                     
-          </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </body>
+    </body>
 </html>
