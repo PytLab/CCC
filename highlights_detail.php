dfdc44bda5f2d04a449393d7fef9b68240fb109d
pytlab <shaozhengjiang@gmail.com>
2015-05-23 22:20:50 +0800

    Initial commit

--- highlights_detail.php
+++ highlights_detail.php
@@ -1,22 +1,12 @@
 <?php
-	include_once("top_index.php");
+    include_once("top_index.php");
 ?>
 <head>
     <title><?php echo $high_ttl;?></title>
     <style>
         * { margin:0; padding:0; word-wrap:break-word; }
-        #highlights_content{
-            width: 770px;
-            float: left;
-        }
         #highlights_content h3{
-            width:770px;
-            height: 110px;
-            margin: 0px 0 0 0;
-            padding: 0px 0 0 0px ;
             background:url(<?php echo $sci_highlight;?>) no-repeat;
-            font-weight: bold;
-            float: left;
         }
     </style>
 </head>
@@ -25,20 +15,20 @@
 <html>
     <body>
         <div id="body">
-			<!--左侧-->
-        	<div id="body_left">
-            	<div id="highlights_content">
-                	<h3></h3>
+            <!--左侧-->
+            <div id="body_left">
+                <div id="highlights_content">
+                    <h3></h3>
                     <?php
-                    	include_once("highlights_paper.php");
-					?>
+                        include_once("highlights_paper.php");
+                    ?>
                 </div>
             </div>
             <!--右侧-->
             <?php include_once("body_right.php");?>
         </div>
         <div id="foot">
-        	<?php include_once("footer.php");?>
+            <?php include_once("footer.php");?>
         </div>
     </body>
 </html>
