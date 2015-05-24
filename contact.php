5ded67ddd21f387f7d540bcfe268e540d37fdc97
pytlab <shaozhengjiang@gmail.com>
2015-05-24 20:05:01 +0800

    Initial commit

--- contact.php
+++ contact.php
@@ -1,67 +1,30 @@
 <?php
 	include_once("top_index.php");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Contact Us - CCC</title>
-<style>
-	body{
-		background-image:url(graph/Blog/grey.jpg);
-		background-repeat:repeat;
-	}
-	:hover{
-		-webkit-transition: 0.5s;
-		-moz-transition: 0.5s;
-		-o-transition: 0.5s;
-		transition:  0.5s;
-	}
-	#course_main{
-		margin:0 auto;
-		width:950px;
-		height:auto;
-		margin-top:0px;
-		margin-bottom:20px;
-		padding-bottom:20px;
-		background-color:#FFF;
-		-webkit-box-shadow: #999 0px 0px  5px ;
-		-moz-box-shadow: #999 0px 0px 5px ;
-		box-shadow: #999 0px 0px 5px ;
-		display:table;
-		text-align:center;
-	}
-	#course_title{
-		width:900px;
-		margin:0 auto;
-		height:110px;
-		background-image:url(<?php echo $contact_ttl;?>);
-	}
-	#contact_info{
-		width:880px;
-		height:auto;
-		padding:10px;
-		margin:0 auto;
-		margin-top:20px;
-		background-color:#E9E9D1;
-		-webkit-box-shadow: #999 0px 0px  15px ;
-		-moz-box-shadow: #999 0px 0px 15px ;
-		box-shadow: #999 0px 0px 15px ;
-	}
-</style>
-</head>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title><?php echo $contact;?></title>
+		<style>
+			body{
+				background-image:url(graph/Blog/grey.jpg);
+				background-repeat:repeat;
+			}
+			#course_title{
+				background-image:url(<?php echo $contact_ttl;?>);
+			}
+		</style>
+	</head>
 
-<body>
-	<div id="course_main">
-    	<div id="course_title"></div>
-        <div id="contact_info">
-			<?php
-                include_once("contact_in.php");
-            ?>
-        </div>
-    </div>
-    <div id="foot">
-        	<?php include_once("footer.php");?>
-    </div>
-</body>
+	<body>
+		<div id="course_main">
+	    	<div id="course_title"></div>
+	        <div id="contact_info">
+				<?php include_once("contact_in.php");?>
+	        </div>
+	    </div>
+	    <div id="foot">
+	        <?php include_once("footer.php");?>
+	    </div>
+	</body>
 </html>
--- contact_in.php
+++ contact_in.php
@@ -1,50 +1,48 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>ContactUs</title>
-<style>
-	#contact_main{
-		margin:0 auto;
-		width:720px;
-		height:150px;
-		margin-top:20px;
-		padding:20px;
-		/*background-color:#E9E9D1;*/
-	}
-</style>
-</head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
+        <title>ContactUs</title>
+        <style>
+            #contact_main{
+                margin:0 auto;
+                width:720px;
+                height:150px;
+                margin-top:20px;
+                padding:20px;
+                /*background-color:#E9E9D1;*/
+            }
+        </style>
+    </head>
 
-<body>
-	<div id="contact_main">
-    	<table width="770" border="0" cellspacing="0" cellpadding="0" align="center">
-          <tr align="center">
-            <td height="30" align="center">Prof. Peijun Hu</td>
-            <td height="30">+86 (0)21 64253755</td>
-            <td height="30">peijunhu@ecust.edu.cn</td>
-          </tr>
-          <tr align="center">
-            <td height="30">Prof. Xueqing Gong </td>
-            <td height="30">+86 (0)21 64251101</td>
-            <td height="30">xgong@ecust.edu.cn</td>
-          </tr>
-          <tr align="center">
-            <td height="30">Dr. Xiaoming Cao</td>
-            <td height="30">+86 (0)21 64253457</td>
-            <td height="30">xmcao@ecust.edu.cn</td>
-          </tr>
-          <tr align="center">
-            <td height="30">Dr. Haifeng Wang </td>
-            <td height="30">+86 (0)21 64253453 </td>
-            <td height="30">hfwang@ecust.edu.cn</td>
-          </tr>
-          <tr align="center">
-            <td height="30">Huihui Liu </td>
-            <td>+86 (0)21 64253755</td>
-            <td>liuhuihui@ecust.edu.cn</td>
-          </tr>
-        </table>
-
-    </div>
-</body>
+    <body>
+        <div id="contact_main">
+            <table width="770" border="0" cellspacing="0" cellpadding="0" align="center">
+                <tr align="left">
+                    <td height="30"><?php echo $hu;?></td>
+                    <td height="30">+86 (0)21 64253755</td>
+                    <td height="30"><a href="mailto:peijunhu@ecust.edu.cn">peijunhu@ecust.edu.cn</a></td>
+                </tr>
+                <tr align="left">
+                    <td height="30"><?php echo $gong;?></td>
+                    <td height="30">+86 (0)21 64251101</td>
+                    <td height="30"><a href="mailto:xgong@ecust.edu.cn">xgong@ecust.edu.cn</a></td>
+                </tr>
+                <tr align="left">
+                    <td height="30"><?php echo $cao;?></td>
+                    <td height="30">+86 (0)21 64253457</td>
+                    <td height="30"><a href="mailto:xmcao@ecust.edu.cn">xmcao@ecust.edu.cn</a></td>
+                </tr>
+                <tr align="left">
+                    <td height="30"><?php echo $wang;?></td>
+                    <td height="30">+86 (0)21 64253453 </td>
+                    <td height="30"><a href="mailto:hfwang@ecust.edu.cn">hfwang@ecust.edu.cn</a></td>
+                </tr>
+                <tr align="left">
+                    <td height="30"><?php echo $liu;?></td>
+                    <td>+86 (0)21 64253755</td>
+                    <td><a href="mailto:liuhuihui@ecust.edu.cn">liuhuihui@ecust.edu.cn</a></td>
+                </tr>
+            </table>
+        </div>
+    </body>
 </html>
