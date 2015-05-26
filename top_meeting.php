5f9966b96b89f76a823c2fade39879604f100249
pytlab <shaozhengjiang@gmail.com>
2015-05-26 13:42:54 +0800

    Initial commit

--- top_meeting.php
+++ top_meeting.php
@@ -8,33 +8,35 @@
 ?>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta name="renderer" content="webkit" />
-<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="./css/main.css" rel="stylesheet" type="text/css" />
-<style>
-	#head_bottom_meeting{
-		width:100%;
-		height:30px;
-		background-color:#093f7f;
-	}
-</style>
-</head>
+    <head>
+        <meta name="renderer" content="webkit" />
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link href="./css/main.css" rel="stylesheet" type="text/css" />
+        <link rel="shortcut icon" href="icon.ico" />
+        <style>
+        	#head_bottom_meeting{
+        		width:100%;
+        		height:30px;
+        		background-color:#093f7f;
+        	}
+        </style>
+    </head>
 
-<body >
-	<div id="main">
-    	<div id="head">
-        	<div id="head_top">
-            	<div id="search">
-					<?php
-                        include_once("search_inweb.html");
-                    ?>
+    <body >
+    	<div id="main">
+        	<div id="head">
+            	<div id="head_top">
+                	<div id="search">
+    					<?php
+                            include_once("search_inweb.html");
+                        ?>
+                    </div>
+                    <div id="logo"><a href="index.php"></a></div>
                 </div>
-                <div id="logo"><a href="index.php"></a></div>
+                <div id="head_bottom_meeting">
+               	</div>
             </div>
-            <div id="head_bottom_meeting">
-           	</div>
         </div>
-       </div>
     </body>
+<html/>
