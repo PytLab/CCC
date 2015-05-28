8ed04ff2932e7b48c72d6b14a6ed02776979782b
ShaoZhengjiang <shaozhengjiang@gmail.com>
2015-05-28 14:37:13 +0800

    Update conn.php

--- conn/conn.php
+++ conn/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn=mysql_connect("localhost", "root", "5098");    //链接数据库服务器
+	$conn=mysql_connect("localhost", "root", "***");     //链接数据库服务器
 	mysql_select_db("db_community",$conn);               //链接数据库
 	mysql_query("set names 'utf8'");                     //对数据库中的编码格式进行转换，避免出现乱码
 ?>
