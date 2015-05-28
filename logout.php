13e1ebffd6a7e6c7ecc075d09a1bb2b02a08bd5c
pytlab <shaozhengjiang@gmail.com>
2015-05-28 11:10:35 +0800

    Initial commit

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
-session_unregister("unc");
-echo "<script>window.location.href='index.php';</script>";
+    session_start();
+    session_unregister("unc");
+    echo "<script>window.location.href='home';</script>";
 ?>
