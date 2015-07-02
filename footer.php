7f7ff4c1c1ebd4a32f403839c9c9f6a338b34de9
PytLab <shaozhengjiang@gmail.com>
2015-07-02 21:12:39 +0800

    Modify href bugs

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <div id="footer_wrapper" class="clearfix">
     <div id="footer">
         <ul>
-            <li><a href="index.php"><?php echo $home?></a></li>
-            <li><a href="people.php"><?php echo $people?></a></li>
-            <li><a href="/features/"><?php echo $news?></a></li>
-            <li><a href="/download/"><?php echo $highlights?></a></li>
-            <li><a href="/pro/"><?php echo $pubs?></a></li>
-            <li><a href="/doc/"><?php echo $courses?></a></li>
-            <li><a href="/doc/"><?php echo $meeting?></a></li>
-            <li><a href="/contact/"><?php echo $contact?></a></li>
+            <li><a href="home"><?php echo $home?></a></li>
+            <li><a href="people"><?php echo $people?></a></li>
+            <li><a href="news"><?php echo $news?></a></li>
+            <li><a href="highlights"><?php echo $highlights?></a></li>
+            <li><a href="publications"><?php echo $pubs?></a></li>
+            <li><a href="courses"><?php echo $courses?></a></li>
+            <li><a href="meeting"><?php echo $meeting?></a></li>
+            <li><a href="contact"><?php echo $contact?></a></li>
         </ul>
         <p class="copyright">&copy; Copyright <a href="home" target="_blank">Center for Computational Chemistry at ECUST</a>. All rights reserved.</p>
     </div>
